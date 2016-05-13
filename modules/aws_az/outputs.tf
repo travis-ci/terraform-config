962bda4b668ec029f9753d613ae9a3e9805e73db
Igor Wiedler <igor@travis-ci.org>
2016-05-13 17:20:37 +0200

    create autoscaling groups via terraform!

--- modules/aws_az/outputs.tf
+++ modules/aws_az/outputs.tf
@@ -5,3 +5,11 @@ output "bastion_eip" {
 output "nat_eip" {
     value = "${aws_eip.nat.public_ip}"
 }
+
+output "workers_subnet_id" {
+    value = "${aws_subnet.workers.id}"
+}
+
+output "workers_security_group_id" {
+    value = "${aws_security_group.workers.id}"
+}
