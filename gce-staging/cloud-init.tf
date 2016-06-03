fa7fc8dba1ce6451ee221e2f6c76cbc8e4e182a3
Igor Wiedler <igor@travis-ci.org>
2016-06-03 20:36:28 +0200

    use new keychain layout

--- gce-staging/cloud-init.tf
+++ gce-staging/cloud-init.tf
@@ -2,7 +2,7 @@ resource "template_file" "cloud_init_org" {
     template = "${file("${path.module}/cloud-init.tpl")}"
 
     vars {
-        account_json = "${file("${path.module}/config/gce-account.json")}"
+        account_json = "${file("${path.module}/config/gce-workers-staging.json")}"
         worker_config = "${file("${path.module}/config/worker-env-org")}"
         chef_json = "${file("${path.module}/config/chef-org.json")}"
         ssh_keys = "${file("${path.module}/config/authorized_keys")}"
@@ -13,7 +13,7 @@ resource "template_file" "cloud_init_com" {
     template = "${file("${path.module}/cloud-init.tpl")}"
 
     vars {
-        account_json = "${file("${path.module}/config/gce-account.json")}"
+        account_json = "${file("${path.module}/config/gce-workers-staging.json")}"
         worker_config = "${file("${path.module}/config/worker-env-com")}"
         chef_json = "${file("${path.module}/config/chef-com.json")}"
         ssh_keys = "${file("${path.module}/config/authorized_keys")}"
