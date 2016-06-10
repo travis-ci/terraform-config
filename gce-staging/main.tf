9aec57377d56a07a79d6f7868a0b7c26572907cf
Igor Wiedler <igor@travis-ci.org>
2016-06-10 16:20:23 +0200

    clear staging-2 project

--- gce-staging/main.tf
+++ gce-staging/main.tf
@@ -12,18 +12,3 @@ module "gce_project_1" {
     cloud_init_org = "${template_file.cloud_init_org.rendered}"
     cloud_init_com = "${template_file.cloud_init_com.rendered}"
 }
-
-module "gce_project_2" {
-    source = "../modules/gce_project"
-
-    env = "${var.env}"
-    index = "2"
-
-    gce_project = "travis-staging-2"
-    gce_bastion_image = "${var.gce_bastion_image}"
-    gce_nat_image = "${var.gce_nat_image}"
-    gce_worker_image = "${var.gce_worker_image}"
-
-    cloud_init_org = "${template_file.cloud_init_org.rendered}"
-    cloud_init_com = "${template_file.cloud_init_com.rendered}"
-}
