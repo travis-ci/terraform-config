39927818eadb86df3c24eeb52bf658a0c879a8ad
Dan Buch <daniel.buch@gmail.com>
2016-09-21 11:44:48 -0400

    Start filling in aws-production-1

---
+++ aws-production-1/aws.tf
@@ -0,0 +1,13 @@
+provider "aws" {}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.2.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.env}-main"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.main.id}"
+}
