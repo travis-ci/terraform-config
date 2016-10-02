1092bc903315dfb6844ebf49b3356c2c9494a833
Dan Buch <daniel.buch@gmail.com>
2016-10-01 23:58:59 -0400

    Add local development env
    for testing out rabbitmq_user module (for now)

---
+++ local-development-0/main.tf
@@ -0,0 +1,14 @@
+variable "rabbitmq_admin_password" { default = "guest" }
+variable "rabbitmq_admin_username" { default = "guest" }
+variable "rabbitmq_host" {}
+variable "rabbitmq_username" { default = "test" }
+variable "rabbitmq_vhost" { default = "/" }
+
+module "rabbitmq_config_test" {
+  source = "../modules/rabbitmq_user"
+  admin_password = "${var.rabbitmq_admin_password}"
+  admin_username = "${var.rabbitmq_admin_username}"
+  endpoint = "${var.rabbitmq_host}"
+  username = "test"
+  vhost = "/"
+}
--- terraform.mk
+++ terraform.mk
@@ -1,29 +1,4 @@
-ENV_NAME := $(notdir $(shell cd $(PWD) && pwd))
-ENV_SHORT ?= $(word 2,$(subst -, ,$(ENV_NAME)))
-INFRA ?= $(word 1,$(subst -, ,$(ENV_NAME)))
-TFVARS := $(PWD)/terraform.tfvars
-TFSTATE := $(PWD)/.terraform/terraform.tfstate
-TFPLAN := $(PWD)/$(ENV_NAME).tfplan
-TFCONF := $(PWD)/.terraform/configured
-
-.PHONY: hello
-hello: announce
-	@echo "Hello there, human."
-	@echo "Would you like to:"
-	@echo "  make plan  - plan your demise"
-	@echo "  make apply - dance with the devil in the pale moonlight"
-
-.PHONY: announce
-announce:
-	@echo "👋 🎉  This is env=$(ENV_NAME) (short=$(ENV_SHORT) infra=$(INFRA))"
-
-.PHONY: apply
-apply: announce .config $(TFVARS) $(TFSTATE)
-	terraform apply $(TFPLAN)
-
-.PHONY: plan
-plan: announce .config $(TFVARS) $(TFSTATE)
-	terraform plan -module-depth=-1 -out=$(TFPLAN)
+include $(shell git rev-parse --show-toplevel)/terraform-common.mk
 
 $(TFCONF): .config $(TFVARS)
 	terraform remote config \
@@ -33,21 +8,6 @@ $(TFCONF): .config $(TFVARS)
 	    -backend-config=region=us-east-1 \
 	    -backend-config=encrypt=true
 	touch $@
-
-$(TFSTATE): $(TFCONF)
-	terraform get
-
-.PHONY: clean
-clean: announce
-	$(RM) -r config $(TFVARS) $(TFCONF)
-
-.PHONY: distclean
-distclean: clean
-	$(RM) -r .terraform/
-
-.PHONY: graph
-graph:
-	terraform graph -draw-cycles | dot -Tpng > graph.png
 
 $(TFVARS):
 	trvs generate-config -f json terraform-config $(subst -,_,$(ENV_NAME)) >$@
