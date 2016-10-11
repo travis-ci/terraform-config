3e7be581dddfc0c245cc8953ba13b9d29764e13a
Dan Buch <daniel.buch@gmail.com>
2016-10-11 16:56:06 -0400

    Update GCE images in gce-staging-1
    and collapse `*.tf` per latest style

--- gce-staging-1/cloud-init.tf
+++
@@ -1,7 +0,0 @@
-data "template_file" "vault_consul_cloud_init" {
-  template = "${file("${path.module}/vault-consul-init.tpl")}"
-
-  vars {
-    vault_consul_config = "${file("${path.module}/config/vault-consul-env")}"
-  }
-}
--- gce-staging-1/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "env" { default = "staging" }
-variable "index" { default = 1 }
-variable "gce_bastion_image" {}
-variable "gce_nat_image" {}
-variable "gce_worker_image" {}
-variable "gce_vault_consul_image" {}
-variable "gce_gcloud_zone" {}
-variable "gce_heroku_org" {}
-variable "job_board_url" {}
