49da097cbcd80ffce00eaf6abe148f4d1896b7fa
Dan Buch <daniel.buch@gmail.com>
2016-10-13 11:33:49 -0400

    Compare docker images more accurately
    and extract more shell stuff out of Makefile land

--- aws.mk
+++ aws.mk
@@ -22,51 +22,17 @@ CONFIG_FILES := \
 .config: $(CONFIG_FILES) $(ENV_NAME).tfvars
 
 $(CONFIG_FILES):
-	mkdir -p config
-	trvs generate-config --pro -p travis_worker -f env $(INFRA)-workers $(ENV_SHORT) \
-		| sed 's/^/export /' >config/worker-com.env
-	trvs generate-config -p travis_worker -f env $(INFRA)-workers $(ENV_SHORT) \
-		| sed 's/^/export /' >config/worker-org.env
-	$(TOP)/bin/heroku-dump-shell-config travis-$(ENV_SHORT) >config/travis-org.env
-	$(TOP)/bin/heroku-dump-shell-config travis-build-$(ENV_SHORT) >config/travis-build-org.env
-	$(TOP)/bin/heroku-dump-shell-config travis-pro-$(ENV_SHORT) >config/travis-com.env
-	$(TOP)/bin/heroku-dump-shell-config travis-pro-build-$(ENV_SHORT) >config/travis-build-com.env
-	source config/travis-build-com.env && source config/travis-com.env && \
-		echo 'export TRAVIS_WORKER_BUILD_API_URI=https://'"$${API_TOKEN%%,*}"'@$(TRAVIS_BUILD_COM_HOST)/script' \
-			>config/worker-com-local.env && \
-		echo 'export TRAVIS_WORKER_AMQP_URI='$${$(AMQP_URL_VARNAME)} >>config/worker-com-local.env && \
-		$(TOP)/bin/env-url-to-parts $(AMQP_URL_VARNAME) config/ com
-	source config/travis-build-org.env && source config/travis-org.env && \
-		echo 'export TRAVIS_WORKER_BUILD_API_URI=https://'"$${API_TOKEN%%,*}"'@$(TRAVIS_BUILD_ORG_HOST)/script' \
-			>config/worker-org-local.env && \
-		echo 'export TRAVIS_WORKER_AMQP_URI='$${$(AMQP_URL_VARNAME)} >>config/worker-org-local.env && \
-		$(TOP)/bin/env-url-to-parts $(AMQP_URL_VARNAME) config/ org
-
-.PHONY: show-current-docker-images
-show-current-docker-images:
-	@terraform show \
-		| awk '/^export.+DOCKER_IMAGE/ { \
-			gsub(/"/, "", $$2); \
-			gsub(/=/, " ", $$2); \
-			sub(/TRAVIS_WORKER_DOCKER_IMAGE_/, "", $$2); \
-			print $$2 " " $$3 \
-		}' \
-		| tr '[:upper:]' '[:lower:]' \
-		| sort \
-		| uniq
-
-.PHONY: show-proposed-docker-images
-show-proposed-docker-images:
-	@awk '/docker_image/ { \
-		gsub(/"/, "", $$3); \
-		sub(/.+docker_image_/, "", $$1); \
-		print $$1 " " $$3 \
-	}' $(ENV_NAME).tfvars
+	$(TOP)/bin/write-aws-config-files \
+		"$(INFRA)" \
+		"$(ENV_SHORT)" \
+		"$(TRAVIS_BUILD_COM_HOST)" \
+		"$(TRAVIS_BUILD_ORG_HOST)" \
+		"$(AMQP_URL_VARNAME)"
 
 .PHONY: diff-docker-images
 diff-docker-images:
 	@diff -u \
 		--label a/docker-images \
-		<($(MAKE) show-current-docker-images) \
+		<($(TOP)/bin/show-current-docker-images) \
 		--label b/docker-images \
-		<($(MAKE) show-proposed-docker-images)
+		<($(TOP)/bin/show-proposed-docker-images "$(ENV_NAME).tfvars")
