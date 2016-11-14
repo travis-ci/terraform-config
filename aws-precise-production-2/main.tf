f1d037292b372f59e3fa2d346898dd41ae8c6772
Dan Buch <daniel.buch@gmail.com>
2016-11-14 12:02:17 -0500

    Expand production to include us-east-1c

--- aws-precise-production-2/main.tf
+++ aws-precise-production-2/main.tf
@@ -82,6 +82,15 @@ module "aws_az_1b" {
   vpc_id = "${data.terraform_remote_state.vpc.vpc_id}"
 }
 
+module "aws_az_1c" {
+  source = "../modules/aws_workers_az"
+  az = "1c"
+  bastion_security_group_id = "${data.terraform_remote_state.vpc.bastion_security_group_1c_id}"
+  env = "${var.env}"
+  index = "${var.index}"
+  vpc_id = "${data.terraform_remote_state.vpc.vpc_id}"
+}
+
 module "aws_az_1e" {
   source = "../modules/aws_workers_az"
   az = "1e"
@@ -102,7 +111,7 @@ module "aws_asg_com" {
   github_users = "${var.github_users}"
   heroku_org = "${var.aws_heroku_org}"
   index = "${var.index}"
-  security_groups = "${module.aws_az_1b.workers_com_security_group_id},${module.aws_az_1e.workers_com_security_group_id}"
+  security_groups = "${module.aws_az_1b.workers_com_security_group_id},${module.aws_az_1c.workers_com_security_group_id},${module.aws_az_1e.workers_com_security_group_id}"
   site = "com"
   syslog_address = "${var.syslog_address_com}"
   worker_ami = "${var.worker_ami}"
@@ -127,7 +136,7 @@ module "aws_asg_com" {
   worker_docker_self_image = "quay.io/travisci/worker:v2.5.0-8-g19ea9c2"
   worker_instance_type = "c3.8xlarge"
   worker_queue = "docker"
-  worker_subnets = "${data.terraform_remote_state.vpc.workers_com_subnet_1b_id},${data.terraform_remote_state.vpc.workers_com_subnet_1e_id}"
+  worker_subnets = "${data.terraform_remote_state.vpc.workers_com_subnet_1b_id},${data.terraform_remote_state.vpc.workers_com_subnet_1c_id},${data.terraform_remote_state.vpc.workers_com_subnet_1e_id}"
 }
 
 module "aws_asg_org" {
@@ -141,7 +150,7 @@ module "aws_asg_org" {
   github_users = "${var.github_users}"
   heroku_org = "${var.aws_heroku_org}"
   index = "${var.index}"
-  security_groups = "${module.aws_az_1b.workers_org_security_group_id},${module.aws_az_1e.workers_org_security_group_id}"
+  security_groups = "${module.aws_az_1b.workers_org_security_group_id},${module.aws_az_1c.workers_org_security_group_id},${module.aws_az_1e.workers_org_security_group_id}"
   site = "org"
   syslog_address = "${var.syslog_address_org}"
   worker_ami = "${var.worker_ami}"
@@ -166,5 +175,5 @@ module "aws_asg_org" {
   worker_docker_self_image = "quay.io/travisci/worker:v2.5.0-8-g19ea9c2"
   worker_instance_type = "c3.8xlarge"
   worker_queue = "docker"
-  worker_subnets = "${data.terraform_remote_state.vpc.workers_org_subnet_1b_id},${data.terraform_remote_state.vpc.workers_org_subnet_1e_id}"
+  worker_subnets = "${data.terraform_remote_state.vpc.workers_org_subnet_1b_id},${data.terraform_remote_state.vpc.workers_org_subnet_1c_id},${data.terraform_remote_state.vpc.workers_org_subnet_1e_id}"
 }
