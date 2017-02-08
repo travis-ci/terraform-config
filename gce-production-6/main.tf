04ad1eda9d778106724e971e535216263eeebbee
Dan Buch <daniel.buch@gmail.com>
2017-02-08 16:40:30 -0500

    Increase gce-production-6 com worker count to 12

--- gce-production-6/main.tf
+++ gce-production-6/main.tf
@@ -9,12 +9,8 @@ variable "travisci_net_external_zone_id" { default = "Z2RI61YP4UWSIO" }
 variable "syslog_address_com" {}
 variable "syslog_address_org" {}
 
-provider "google" {
-  project = "travis-ci-prod-6"
-}
-
+provider "google" { project = "travis-ci-prod-6" }
 provider "aws" {}
-
 provider "heroku" {}
 
 module "gce_project_6" {
@@ -22,10 +18,10 @@ module "gce_project_6" {
   bastion_config = "${file("${path.module}/config/bastion-env")}"
   bastion_image = "${var.gce_bastion_image}"
   env = "${var.env}"
-  github_users = "${var.github_users}"
   gcloud_cleanup_account_json = "${file("${path.module}/config/gce-cleanup-production-6.json")}"
   gcloud_cleanup_job_board_url = "${var.job_board_url}"
   gcloud_zone = "${var.gce_gcloud_zone}"
+  github_users = "${var.github_users}"
   heroku_org = "${var.gce_heroku_org}"
   index = "6"
   project = "travis-ci-prod-6"
@@ -38,7 +34,6 @@ module "gce_project_6" {
   worker_config_org = "${file("${path.module}/config/worker-env-org")}"
   worker_docker_self_image = "travisci/worker:v2.7.0"
   worker_image = "${var.gce_worker_image}"
-  # instance count must be a multiple of number of zones (currently 2)
-  worker_instance_count_com = 2
-  worker_instance_count_org = 2
+  worker_instance_count_com = 12
+  worker_instance_count_org = 0
 }
