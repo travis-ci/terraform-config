50f98fb8741b606b52e801b07b85a885db936001
Dan Buch <daniel.buch@gmail.com>
2017-06-18 07:43:30 -0400

    Roll back GCE and MacStadium to previous worker versions

--- macstadium-shared-1/main.tf
+++ macstadium-shared-1/main.tf
@@ -43,35 +43,35 @@ variable "jupiter_brain_staging_version" {
 }
 
 variable "travis_worker_custom-1_version" {
-  default = "v2.9.2"
+  default = "v2.6.2"
 }
 
 variable "travis_worker_custom-2_version" {
-  default = "v2.9.2"
+  default = "v2.6.2"
 }
 
 variable "travis_worker_custom-3_version" {
-  default = "v2.9.2"
+  default = "v2.6.2"
 }
 
 variable "travis_worker_custom-4_version" {
-  default = "v2.9.2"
+  default = "v2.6.2"
 }
 
 variable "travis_worker_custom-5_version" {
-  default = "v2.9.2"
+  default = "v2.6.2"
 }
 
 variable "travis_worker_custom-6_version" {
-  default = "v2.9.2"
+  default = "v2.6.2"
 }
 
 variable "travis_worker_production_version" {
-  default = "v2.9.2"
+  default = "v2.6.2"
 }
 
 variable "travis_worker_staging_version" {
-  default = "v2.9.2"
+  default = "v2.6.2"
 }
 
 variable "vsphere_janitor_version" {
