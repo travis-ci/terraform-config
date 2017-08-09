576e6b225cd6c024524093b3b6fa73b750503da0
AJ Bowen <aj@soulshake.net>
2017-08-09 11:46:17 +0200

    Add 'make list' to list all make targets

--- terraform-common.mk
+++ terraform-common.mk
@@ -57,6 +57,9 @@ graph:
 $(ENV_NAME).tfvars:
 	$(TOP)/bin/generate-tfvars $@
 
+.PHONY: list
+list:
+	 @$(MAKE) -pRrq -f $(lastword $(MAKEFILE_LIST)) : 2>/dev/null | awk -v RS= -F: '/^# File/,/^# Finished Make data base/ {if ($$1 !~ "^[#.]") {print $$1}}' | sort | egrep -v -e '^[^[:alnum:]]' -e '^$@$$' | xargs
 
 .PHONY: check
 check:
