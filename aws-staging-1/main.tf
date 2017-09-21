aba0d55ad7fcd53b9eb2f14507cc54ecf1a878bf
AJ Bowen <aj@soulshake.net>
2017-09-21 15:46:49 +0200

    Use overlay2

--- aws-staging-1/main.tf
+++ aws-staging-1/main.tf
@@ -73,6 +73,7 @@ ${file("${path.module}/config/worker-com.env")}
 ${file("${path.module}/worker.env")}
 
 export TRAVIS_WORKER_TRAVIS_SITE=com
+export TRAVIS_DOCKER_DISABLE_DIRECT_LVM=1
 EOF
 }
 
@@ -86,6 +87,7 @@ ${file("${path.module}/config/worker-org.env")}
 ${file("${path.module}/worker.env")}
 
 export TRAVIS_WORKER_TRAVIS_SITE=org
+export TRAVIS_DOCKER_DISABLE_DIRECT_LVM=1
 EOF
 }
 
