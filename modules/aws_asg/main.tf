d89575921a2f54118a30e6068f7304055d372f64
Dan Buch <daniel.buch@gmail.com>
2017-10-03 12:49:59 -0400

    Ensure worker v2.11.0 rollback sticks

--- modules/aws_asg/main.tf
+++ modules/aws_asg/main.tf
@@ -115,7 +115,7 @@ variable "worker_docker_image_python" {}
 variable "worker_docker_image_ruby" {}
 
 variable "worker_docker_self_image" {
-  default = "travisci/worker:v3.0.2"
+  default = "travisci/worker:v2.11.0"
 }
 
 variable "worker_instance_type" {
