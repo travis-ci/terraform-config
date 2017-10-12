f11c1a1d2409219dec08e67222b4aa22aed0abc0
Katherine Daniels <kdaniels@travis-ci.org>
2017-10-12 16:37:42 -0400

    Adding directories for new Base VMs for macstadium migration

--- modules/macstadium_infrastructure/folder_structure.tf
+++ modules/macstadium_infrastructure/folder_structure.tf
@@ -3,6 +3,26 @@ resource "vsphere_folder" "base_vms" {
   datacenter = "${var.datacenter}"
 }
 
+resource "vsphere_folder" "datacore1_base_vms" {
+  path       = "DATACORE 1 Base VMs"
+  datacenter = "${var.datacenter}"
+}
+
+resource "vsphere_folder" "datacore2_base_vms" {
+  path       = "DATACORE 2 Base VMs"
+  datacenter = "${var.datacenter}"
+}
+
+resource "vsphere_folder" "datacore3_base_vms" {
+  path       = "DATACORE 3 Base VMs"
+  datacenter = "${var.datacenter}"
+}
+
+resource "vsphere_folder" "datacore4_base_vms" {
+  path       = "DATACORE 4 Base VMs"
+  datacenter = "${var.datacenter}"
+}
+
 resource "vsphere_folder" "build_vms" {
   path       = "Build VMs"
   datacenter = "${var.datacenter}"
