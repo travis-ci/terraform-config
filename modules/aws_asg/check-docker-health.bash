e4ccedc93818c2d7ac8c774428485a655742fa65
AJ Bowen <aj@soulshake.net>
2017-11-04 09:44:20 -0700

    - Rename local post_sleep to pre_implosion_sleep to reflect how it's used in this script (although it still inherits from POST_SHUTDOWN_SLEEP). - Reword implosion messages for easier greppery. - Post shutdown sleep is pointless here; remove it.

--- modules/aws_asg/check-docker-health.bash
+++ modules/aws_asg/check-docker-health.bash
@@ -7,16 +7,16 @@ set -o pipefail
 
 main() {
   local warmup_grace_period=600
-  local post_sleep="${POST_SHUTDOWN_SLEEP}"
+  local pre_implosion_sleep="${POST_SHUTDOWN_SLEEP}"
   local sleep_time="${DOCKER_PS_SLEEP_TIME}"
   local run_d="${RUNDIR}"
-  : "${post_sleep:=300}"
+  : "${pre_implosion_sleep:=300}"
   : "${sleep_time:=5}"
   : "${run_d:=/var/tmp/travis-run.d}"
   : "${KILL_COMMAND:=kill}"
 
   if [[ -f "${run_d}/implode.confirm" ]]; then
-    __handle_implode_confirm "${run_d}" "${post_sleep}"
+    __handle_implode_confirm "${run_d}" "${pre_implosion_sleep}"
     __die imploded 42
   fi
 
@@ -45,26 +45,25 @@ main() {
 
 __handle_implode_confirm() {
   local run_d="${1}"
-  local post_sleep="${2}"
+  local pre_implosion_sleep="${2}"
 
   local reason
   reason="$(cat "${run_d}/implode.confirm" 2>/dev/null)"
   : "${reason:=not sure why}"
   "${SHUTDOWN:-/sbin/shutdown}" -P now "imploding because ${reason}"
-  sleep "${post_sleep}"
 }
 
 __handle_unresponsive_docker() {
   local run_d="${1}"
-  msg="docker appears to be unhealthy"
+  msg="docker appears to be unhealthy, initiating implosion"
   echo "$msg" | tee "${run_d}/implode"
   logger "$msg"
 
-  logger "Sleeping ${post_sleep}"
-  sleep "${post_sleep}"
+  logger "Sleeping ${pre_implosion_sleep}"
+  sleep "${pre_implosion_sleep}"
 
   if [ ! -e "${run_d}/implode" ]; then
-    logger "${run_d}/implode not found, not imploding?"
+    logger "docker previously reported as unhealthy, but ${run_d}/implode not found; not imploding?"
     __die noop 0
   fi
 
@@ -74,7 +73,7 @@ __handle_unresponsive_docker() {
     echo "$msg" | tee "${run_d}/implode.confirm"
     logger "$msg"
   else
-    logger "Running '${KILL_COMMAND} -TERM $pid' to kill travis-worker."
+    logger "Running '${KILL_COMMAND} -TERM $pid' to kill travis-worker due to unhealthy docker."
     "${KILL_COMMAND}" -TERM "$pid"
   fi
 }
