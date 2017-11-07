b7fda97ba4dfc75dba1a629a9a9f07a5d4c7a60c
Lyoness <carmen.andoh@gmail.com>
2017-11-07 10:03:12 -0500

    Rename S3 key to include v0.9.11 pin.

--- macstadium-shared-1/main.tf
+++ macstadium-shared-1/main.tf
@@ -87,7 +87,7 @@ variable "vsphere_ip" {}
 terraform {
   backend "s3" {
     bucket         = "travis-terraform-state"
-    key            = "terraform-config/macstadium-shared-1.tfstate"
+    key            = "terraform-config/macstadium-shared-1-terraform-v0.9.11.tfstate"
     region         = "us-east-1"
     encrypt        = "true"
     dynamodb_table = "travis-terraform-state"
