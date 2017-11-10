c89d740dade201119e83f2b1cc966892d894c2b2
AJ Bowen <aj@soulshake.net>
2017-11-10 15:48:01 -0800

    Just kill all containers older than travis-worker

--- modules/aws_asg/kill-old-containers.bash
+++ modules/aws_asg/kill-old-containers.bash
@@ -1,21 +1,14 @@
 #!/bin/bash
+set -e
 
-for cid in $(docker ps -q); do
-  now=$(date +%s)
+main() {
+  worker_cid=$(docker inspect travis-worker --format '{{ .Id }}')
+  cids=$(docker ps -q --filter before="$worker_cid")
 
-  created_at=$(date -d "$(docker inspect --format '{{.Created}}' "$cid")" +%s)
-  cutoff=$(echo "$(date +%s)-60*120" | bc)
-  ttl=$((created_at - cutoff))
-  name="$(docker inspect "$cid" --format '{{ .Name }}')"
-  age=$((now - created_at))
+  for cid in $cids; do
+    logger "Removing container older than travis-worker: $(docker ps --filter id="$cid" | grep -v CONTAINER)"
+    docker kill "$cid"
+  done
+}
 
-  if [ $ttl -lt 0 ]; then
-    if [[ "$name" == "/travis-worker" ]]; then
-      echo " [OK] travis-worker"
-      continue
-    fi
-    echo "[NOK] $cid: OLD! $(date -d@$age +%H:%M:%S) => $name"
-  else
-    echo " [OK] $cid: $(date -d@$ttl -u +%H:%M:%S) left => $name"
-  fi
-done
+main "$@"
