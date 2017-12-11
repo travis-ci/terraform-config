e50fadaf18e484d2cb81d935e47967321baf5623
Dan Buch <daniel.buch@gmail.com>
2017-12-11 11:20:26 -0500

    Blooping at NAT stuff some more

--- modules/packet_network/cloud-init.bash
+++ modules/packet_network/cloud-init.bash
@@ -37,17 +37,20 @@ __setup_sysctl() {
   # play nicely with others, such as when multiple containers are running mysql,
   # which is the default on precise + trusty.  The value we set here is 16^5,
   # which is one power higher than the default of 16^4 :sparkles:.
+  echo 1048576 >/proc/sys/fs/aio-max-nr
   sysctl -w fs.aio-max-nr=1048576
 
+  echo 1 >/proc/sys/net/ipv4/ip_forward
   sysctl -w net.ipv4.ip_forward=1
 }
 
 __setup_iptables() {
-  # FIXME: how to do this with a bonded interface?
-  local pub_iface priv_iface
+  local pub_iface priv_iface elastic_ip
   pub_iface="$(__find_public_interface)"
   priv_iface="$(__find_private_interface)"
+  elastic_ip="$(__find_elastic_ip)"
 
+  iptables -t nat -A POSTROUTING -o "${pub_iface}" -j SNAT --to "${elastic_ip}"
   iptables -t nat -A POSTROUTING -o "${pub_iface}" -j MASQUERADE
   iptables -A FORWARD -i "${pub_iface}" -o "${priv_iface}" \
     -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
@@ -56,12 +59,17 @@ __setup_iptables() {
 
 __find_private_interface() {
   # FIXME: dynamically do
-  echo bond0:0
+  echo enp2s0d1
 }
 
 __find_public_interface() {
   # FIXME: dynamically do
   echo bond0
+}
+
+__find_elastic_ip() {
+  # FIXME: inject this from somewhere?
+  echo 127.0.0.1
 }
 
 main "$@"
