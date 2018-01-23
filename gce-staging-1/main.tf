b02df041c83377e773f4011c9fc89337b61934d7
Dan Buch <dan@meatballhat.com>
2018-01-22 22:06:13 -0500

    Bump to latest tfw
    and adjust firewall deny rule to match valid (?) priority

--- gce-staging-1/main.tf
+++ gce-staging-1/main.tf
@@ -10,7 +10,7 @@ variable "gce_gcloud_zone" {}
 variable "gce_heroku_org" {}
 
 variable "gce_worker_image" {
-  default = "eco-emissary-99515/tfw-1516641023-064b6d3"
+  default = "eco-emissary-99515/tfw-1516675156-0b5be43"
 }
 
 variable "github_users" {}
