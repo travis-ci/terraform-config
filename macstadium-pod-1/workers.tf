6aa825c464589fb4a4049b815e4be627ae2ca9dc
emdantrim <emdantrim@gmail.com>
2018-02-05 15:21:41 -0500

    Fix typo

--- macstadium-pod-1/workers.tf
+++ macstadium-pod-1/workers.tf
@@ -33,7 +33,7 @@ export TRAVIS_WORKER_TRAVIS_SITE="org"
 export TRAVIS_WORKER_POOL_SIZE="210"
 export TRAVIS_WORKER_PPROF_PORT="7070"
 export TRAVIS_WORKER_JUPITERBRAIN_ENDPOINT=http://${random_id.jupiter_brain_production_org_token.hex}@127.0.0.1:8081/
-export TRAVIS_WORKER_LIBRATO_SOURCE="travis-worker-production-org-macstadium-$${index}-1-dc18"
+export TRAVIS_WORKER_LIBRATO_SOURCE="travis-worker-production-org-macstadium-${var.index}-1-dc18"
 EOF
 }
 
