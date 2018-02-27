7831d858702c3dba49bb491a9601b8309e83c37a
AJ Bowen <aj@soulshake.net>
2018-02-26 18:56:49 -0600

    add script to check and log high CPU usage

---
+++ modules/aws_asg/high-cpu-check.bash
@@ -0,0 +1,70 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+__logger() {
+  level="$1" && shift
+  msg="$1" && shift
+  date="$(date --iso-8601=seconds)"
+  log_msg="tag=cron time=$date level=$level msg=\"$msg\" $*"
+  logger -t "$(basename "${0}")" "$log_msg"
+}
+
+__die() {
+  local status="${1}"
+  local code="${2}"
+  __logger "info" \
+    "cron finished" \
+    "status=${status}"
+  exit "${code}"
+}
+
+report_greedy_containers() {
+  max_cpu="$1"
+  : "${RUNDIR:=/var/tmp/travis-run.d}"
+  instance_id="$(cat "${RUNDIR}/instance-id")"
+  instance_ip="$(cat "${RUNDIR}/instance-ipv4")"
+
+  IFS=$(echo -en "\n\b")
+  cpu_usage="$(docker stats --no-stream --format "{{.Container}} {{.CPUPerc}} {{.Name}}")"
+
+  for line in $cpu_usage; do
+
+    IFS=" "
+    echo "$line" | while read -r cid usage_as_float name; do
+      if [[ "${name}" == "travis-worker" ]]; then
+        continue
+      fi
+
+      usage_as_int=${usage_as_float%.*}
+      if [ "$usage_as_int" -gt "${max_cpu}" ]; then
+        echo "$cid $usage_as_int $name"
+        __logger "warning" \
+          "high cpu usage detected" \
+          "status=noop" \
+          "instance_id=${instance_id}" \
+          "instance_ip=${instance_ip}" \
+          "cid=$cid" \
+          "cpu_usage=$usage_as_float" \
+          "name=$name"
+      fi
+    done
+  done
+}
+
+main() {
+  # shellcheck disable=SC2153
+  : "${MAX_CPU:=12000}"
+
+  local max_cpu
+  max_cpu="${MAX_CPU}"
+
+  {
+    IFS=$(echo -en "\n\b")
+    for greedy_cid in $(report_greedy_containers "${max_cpu}"); do
+      echo "greedy cid: $greedy_cid"
+    done
+  }
+}
+
+main "$@"
