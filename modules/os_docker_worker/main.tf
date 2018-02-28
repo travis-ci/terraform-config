04d2059d4190e445d0d6bc9a654460ebbd5ed354
spurti-chopra <spurti@us.ibm.com>
2018-02-28 17:54:37 -0500

    Added terraform plan for openstack docker and respective module

---
+++ modules/os_docker_worker/main.tf
@@ -0,0 +1,47 @@
+variable "instance_count" {}
+variable "index" {}
+variable "env" {}
+variable "worker_config" {}
+variable "flavor_name" {}
+variable "network" {}
+variable "security_groups" {}
+variable "worker_image" {}
+variable "provider" {}
+variable "key_name" {}
+variable "docker_storage_dm_basesize" {}
+variable "availability_zone" {}
+
+data "template_file" "docker_daemon_json" {
+  template = <<EOF
+{
+  "storage-driver": "devicemapper",
+  "storage-opts": [
+    "dm.basesize=${var.docker_storage_dm_basesize}"
+  ]
+}
+EOF
+}
+
+data "template_file" "cloud_config" {
+  template = "${file("${path.module}/cloud-config.yml.tpl")}"
+  vars {
+    here               = "${path.module}"
+    worker_config      = "${var.worker_config}"
+    provider           = "${var.provider}"
+    docker_daemon_json = "${data.template_file.docker_daemon_json.rendered}"
+  }
+}
+
+resource "openstack_compute_instance_v2" "worker" {
+  count             = "${var.instance_count}"
+  name              = "${format("${var.env}-${var.index}-${var.provider}-worker-%02d", count.index + 1)}"
+  image_name        = "${var.worker_image}"
+  flavor_name       = "${var.flavor_name}"
+  key_pair          = "${var.key_name}"
+  security_groups   = ["${var.security_groups}"]
+  network {
+    name = "${var.network}"
+  }
+  availability_zone = "${var.availability_zone}"
+  user_data         = "${data.template_file.cloud_config.rendered}"
+}
