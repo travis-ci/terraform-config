0a8ed8f045c8b1c2f4272f7b4fd9d4c4be16dbb1
Dan Buch <dan@meatballhat.com>
2018-03-06 20:52:59 -0500

    Migrating gce-production-3 workers-only graph

--- gce-production-3/main.tf
+++ gce-production-3/main.tf
@@ -1,11 +1,5 @@
-variable "deny_target_ip_ranges" {}
-
 variable "env" {
   default = "production"
-}
-
-variable "gce_bastion_image" {
-  default = "https://www.googleapis.com/compute/v1/projects/eco-emissary-99515/global/images/bastion-1496867305"
 }
 
 variable "gce_gcloud_zone" {}
@@ -24,6 +18,12 @@ variable "travisci_net_external_zone_id" {
 
 variable "syslog_address_com" {}
 variable "syslog_address_org" {}
+variable "worker_instance_count_com" {}
+variable "worker_instance_count_org" {}
+
+variable "worker_zones" {
+  default = ["a", "b", "c", "f"]
+}
 
 terraform {
   backend "s3" {
@@ -44,11 +44,20 @@ provider "google" {
 provider "aws" {}
 provider "heroku" {}
 
-module "gce_project_1" {
-  source                        = "../modules/gce_project"
-  bastion_config                = "${file("${path.module}/config/bastion.env")}"
-  bastion_image                 = "${var.gce_bastion_image}"
-  deny_target_ip_ranges         = ["${split(",", var.deny_target_ip_ranges)}"]
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config {
+    bucket         = "travis-terraform-state"
+    key            = "terraform-config/gce-production-net-3.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "travis-terraform-state"
+  }
+}
+
+module "gce_worker_group" {
+  source = "../modules/gce_worker_group"
+
   env                           = "${var.env}"
   gcloud_cleanup_account_json   = "${file("${path.module}/config/gce-cleanup-production-3.json")}"
   gcloud_cleanup_job_board_url  = "${var.job_board_url}"
@@ -57,19 +66,19 @@ module "gce_project_1" {
   heroku_org                    = "${var.gce_heroku_org}"
   index                         = "3"
   project                       = "travis-ci-prod-3"
+  region                        = "us-central1"
   syslog_address_com            = "${var.syslog_address_com}"
   syslog_address_org            = "${var.syslog_address_org}"
   travisci_net_external_zone_id = "${var.travisci_net_external_zone_id}"
   worker_account_json_com       = "${file("${path.module}/config/gce-workers-production-3.json")}"
   worker_account_json_org       = "${file("${path.module}/config/gce-workers-production-3.json")}"
   worker_image                  = "${var.gce_worker_image}"
-  worker_instance_count_com     = 4
-  worker_instance_count_org     = 4
+  worker_subnetwork             = "${data.terraform_remote_state.vpc.gce_subnetwork_workers}"
 
-  build_com_subnet_cidr_range = "10.10.24.0/22"
-  build_org_subnet_cidr_range = "10.10.20.0/22"
-  public_subnet_cidr_range    = "10.10.1.0/24"
-  workers_subnet_cidr_range   = "10.10.16.0/22"
+  worker_zones = "${var.worker_zones}"
+
+  worker_instance_count_com = "${var.worker_instance_count_com}"
+  worker_instance_count_org = "${var.worker_instance_count_org}"
 
   worker_config_com = <<EOF
 ### worker.env
