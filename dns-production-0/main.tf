d8462bced0e16e852a1f414688ff6ad160e66bed
Dan Buch <dan@meatballhat.com>
2018-03-08 09:48:54 -0500

    Rename DNS graph to cover all production infrastructures

--- dns-production-0/main.tf
+++ dns-production-0/main.tf
@@ -9,7 +9,7 @@ variable "region" {
 terraform {
   backend "s3" {
     bucket         = "travis-terraform-state"
-    key            = "terraform-config/gce-production-nat-dns.tfstate"
+    key            = "terraform-config/dns-production-0.tfstate"
     region         = "us-east-1"
     encrypt        = "true"
     dynamodb_table = "travis-terraform-state"
@@ -38,7 +38,7 @@ data "dns_a_record_set" "gce_production_5_nat" {
   host = "nat-production-5.gce-${var.region}.travisci.net"
 }
 
-resource "aws_route53_record" "nat" {
+resource "aws_route53_record" "gce_nat" {
   zone_id = "${var.travisci_net_external_zone_id}"
   name    = "nat.gce-${var.region}.travisci.net"
   type    = "A"
