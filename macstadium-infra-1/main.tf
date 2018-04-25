2999aad98248211cf937f40f8f462798663c06df
emdantrim <emdantrim@gmail.com>
2018-04-25 16:05:51 -0400

    Stub out macstadium-infra-1

---
+++ macstadium-infra-1/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket         = "travis-terraform-state"
+    key            = "terraform-config/macstadium-infra-1.tfstate"
+    region         = "us-east-1"
+    encrypt        = "true"
+    dynamodb_table = "travis-terraform-state"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "vsphere" {
+  user                 = "${var.vsphere_user}"
+  password             = "${var.vsphere_password}"
+  vsphere_server       = "${var.vsphere_server}"
+  allow_unverified_ssl = true
+}
+
