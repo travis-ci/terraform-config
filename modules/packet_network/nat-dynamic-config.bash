24ca825c51d96e7fd3158efe730bc408ae90e474
Dan Buch <dan@meatballhat.com>
2018-04-26 16:15:01 -0400

    Ensure packet spoof rules are happy

--- modules/packet_network/nat-dynamic-config.bash
+++ modules/packet_network/nat-dynamic-config.bash
@@ -93,10 +93,10 @@ __setup_networking() {
     iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
   fi
 
-  if ! iptables -C INPUT --in-interface "${loc_iface}" --src ! "${loc_subnet}" -j LOG --log-prefix "SPOOF"; then
-    iptables -A INPUT --in-interface "${loc_iface}" --src ! "${loc_subnet}" -j LOG --log-prefix "SPOOF"
-    if ! iptables -C INPUT --in-interface "${loc_iface}" --src ! "${loc_subnet}" -j DROP; then
-      iptables -A INPUT --in-interface "${loc_iface}" --src ! "${loc_subnet}" -j DROP
+  if ! iptables -C INPUT --in-interface "${loc_iface}" ! --src "${loc_subnet}" -j LOG --log-prefix "SPOOF"; then
+    iptables -A INPUT --in-interface "${loc_iface}" ! --src "${loc_subnet}" -j LOG --log-prefix "SPOOF"
+    if ! iptables -C INPUT --in-interface "${loc_iface}" ! --src "${loc_subnet}" -j DROP; then
+      iptables -A INPUT --in-interface "${loc_iface}" ! --src "${loc_subnet}" -j DROP
     fi
   fi
 
@@ -108,21 +108,22 @@ __setup_networking() {
 
 __find_public_interface() {
   local iface=bond0
-  iface="$(ip -o addr show | grep -vE 'inet (172|127|10|192)\.' | grep -v inet6 |
-    awk '{ print $2 }' | grep -v '^lo$' | head -n 1)"
+  iface="$(ip -o addr show | grep -vE 'inet (172|127|10|192)\.' |
+    grep -v inet6 | awk '{ print $2 }' | grep -v '^lo$' | head -n 1)"
   echo "${iface:-bond0}"
 }
 
 __find_local_interface() {
   local iface=enp1s0f1
-  subnet="$(ip -o addr show | grep -v inet6 | grep -v " lo " | grep -v "bond" |
-    grep -v "docker" | awk '{ print $2}')"
+  subnet="$(ip -o addr show | grep -vE 'inet6| lo |bond|docker' |
+    awk '{ print $2 }')"
   echo "${iface:-enp1s0f1}"
 }
 
 __find_local_subnet() {
   local subnet="192.168.1.1/24"
-  subnet="$(ip -o addr show "$(__find_local_interface)" | awk '{ print $4}')"
+  subnet="$(ip -o addr show "$(__find_local_interface)" | grep -v inet6 |
+    awk '{ print $4 }')"
   echo "${subnet:-"192.168.1.1/24"}"
 }
 
