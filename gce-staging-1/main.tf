98db9f46ea870bd7e9777c11b939d1edcf225db9
Igor Wiedler <igor@travis-ci.org>
2018-06-21 16:14:32 +0200

    revert tfw to fixed (older) version tfw-1527863525-7808917

--- gce-staging-1/main.tf
+++ gce-staging-1/main.tf
@@ -7,6 +7,10 @@ variable "gce_heroku_org" {}
 variable "latest_gce_tfw_image" {}
 variable "github_users" {}
 
+variable "gce_tfw_image" {
+  default = "https://www.googleapis.com/compute/v1/projects/eco-emissary-99515/global/images/tfw-1527863525-7808917"
+}
+
 variable "index" {
   default = 1
 }
@@ -75,7 +79,7 @@ module "gce_worker_group" {
   worker_account_json_com       = "${file("${path.module}/config/gce-workers-staging-1.json")}"
   worker_account_json_org       = "${file("${path.module}/config/gce-workers-staging-1.json")}"
   worker_docker_self_image      = "${var.latest_docker_image_worker}"
-  worker_image                  = "${var.latest_gce_tfw_image}"
+  worker_image                  = "${var.gce_tfw_image}"
   worker_subnetwork             = "${data.terraform_remote_state.vpc.gce_subnetwork_workers}"
 
   worker_zones = "${var.worker_zones}"
