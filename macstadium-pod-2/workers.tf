cc4ab49127362b73893176db00628dff939344ab
Matt Moriarity <matt@mattmoriarity.com>
2018-09-07 15:15:03 -0500

    Fix typo for worker com tokens

--- macstadium-pod-2/workers.tf
+++ macstadium-pod-2/workers.tf
@@ -125,7 +125,7 @@ export TRAVIS_WORKER_HARD_TIMEOUT=120m
 export TRAVIS_WORKER_TRAVIS_SITE="com"
 export TRAVIS_WORKER_POOL_SIZE="52"
 export TRAVIS_WORKER_PPROF_PORT="7080"
-export TRAVIS_WORKER_HTTP_API_AUTH="${random_id_travis_worker_production_com_token.hex}"
+export TRAVIS_WORKER_HTTP_API_AUTH="${random_id.travis_worker_production_com_token.hex}"
 export TRAVIS_WORKER_JUPITERBRAIN_ENDPOINT="http://${random_id.jupiter_brain_production_com_token.hex}@127.0.0.1:8083/"
 export TRAVIS_WORKER_LIBRATO_SOURCE="travis-worker-production-com-macstadium-${var.index}-1-dc18"
 EOF
@@ -147,7 +147,7 @@ export TRAVIS_WORKER_HARD_TIMEOUT=120m
 export TRAVIS_WORKER_TRAVIS_SITE="com"
 export TRAVIS_WORKER_POOL_SIZE="52"
 export TRAVIS_WORKER_PPROF_PORT="7081"
-export TRAVIS_WORKER_HTTP_API_AUTH="${random_id_travis_worker_production_com_token.hex}"
+export TRAVIS_WORKER_HTTP_API_AUTH="${random_id.travis_worker_production_com_token.hex}"
 export TRAVIS_WORKER_JUPITERBRAIN_ENDPOINT="http://${random_id.jupiter_brain_production_com_token.hex}@127.0.0.1:8083/"
 export TRAVIS_WORKER_LIBRATO_SOURCE="travis-worker-production-com-macstadium-${var.index}-2-dc18"
 EOF
