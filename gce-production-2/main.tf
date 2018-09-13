6407664bd95799068d0b412bd6286beee5766173
Igor <igorwwwwwwwwwwwwwwwwwwww@users.noreply.github.com>
2018-09-13 18:06:17 +0200

    gce-production: enable buildsh tracing (#544)

--- gce-production-2/main.tf
+++ gce-production-2/main.tf
@@ -10,6 +10,11 @@ variable "gce_worker_image" {
 }
 
 variable "github_users" {}
+
+variable "index" {
+  default = 2
+}
+
 variable "job_board_url" {}
 
 variable "project" {
@@ -63,6 +68,20 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
+module "aws_iam_user_s3_com" {
+  source = "../modules/aws_iam_user_s3"
+
+  iam_user_name  = "worker-gce-${var.env}-${var.index}-com"
+  s3_bucket_name = "build-trace.travis-ci.com"
+}
+
+module "aws_iam_user_s3_org" {
+  source = "../modules/aws_iam_user_s3"
+
+  iam_user_name  = "worker-gce-${var.env}-${var.index}-org"
+  s3_bucket_name = "build-trace.travis-ci.org"
+}
+
 module "gce_worker_group" {
   source = "../modules/gce_worker_group"
 
@@ -71,7 +90,7 @@ module "gce_worker_group" {
   gcloud_zone                   = "${var.gce_gcloud_zone}"
   github_users                  = "${var.github_users}"
   heroku_org                    = "${var.gce_heroku_org}"
-  index                         = "2"
+  index                         = "${var.index}"
   project                       = "${var.project}"
   region                        = "us-central1"
   syslog_address_com            = "${var.syslog_address_com}"
@@ -96,6 +115,10 @@ export TRAVIS_WORKER_QUEUE_NAME=builds.gce
 export TRAVIS_WORKER_GCE_SUBNETWORK=jobs-com
 export TRAVIS_WORKER_HARD_TIMEOUT=120m
 export TRAVIS_WORKER_TRAVIS_SITE=com
+
+export TRAVIS_WORKER_BUILD_TRACE_S3_BUCKET=${module.aws_iam_user_s3_com.bucket}
+export AWS_ACCESS_KEY_ID=${module.aws_iam_user_s3_com.id}
+export AWS_SECRET_ACCESS_KEY=${module.aws_iam_user_s3_com.secret}
 EOF
 
   worker_config_com_free = <<EOF
@@ -108,6 +131,10 @@ export TRAVIS_WORKER_QUEUE_NAME=builds.gce-free
 export TRAVIS_WORKER_GCE_SUBNETWORK=jobs-com
 export TRAVIS_WORKER_HARD_TIMEOUT=120m
 export TRAVIS_WORKER_TRAVIS_SITE=com
+
+export TRAVIS_WORKER_BUILD_TRACE_S3_BUCKET=${module.aws_iam_user_s3_com.bucket}
+export AWS_ACCESS_KEY_ID=${module.aws_iam_user_s3_com.id}
+export AWS_SECRET_ACCESS_KEY=${module.aws_iam_user_s3_com.secret}
 EOF
 
   worker_config_org = <<EOF
@@ -119,6 +146,10 @@ ${file("${path.module}/config/worker-org.env")}
 export TRAVIS_WORKER_QUEUE_NAME=builds.gce
 export TRAVIS_WORKER_GCE_SUBNETWORK=jobs-org
 export TRAVIS_WORKER_TRAVIS_SITE=org
+
+export TRAVIS_WORKER_BUILD_TRACE_S3_BUCKET=${module.aws_iam_user_s3_org.bucket}
+export AWS_ACCESS_KEY_ID=${module.aws_iam_user_s3_org.id}
+export AWS_SECRET_ACCESS_KEY=${module.aws_iam_user_s3_org.secret}
 EOF
 }
 
