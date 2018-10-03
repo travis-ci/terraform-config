734e9f6d8edd59053539e49d2cf071ed986e1303
Dan Buch <dan@meatballhat.com>
2018-10-03 12:56:10 -0400

    Bump worker to v4.3.0

--- modules/gce_worker_group/main.tf
+++ modules/gce_worker_group/main.tf
@@ -46,7 +46,7 @@ variable "worker_config_com_free" {}
 variable "worker_config_org" {}
 
 variable "worker_docker_self_image" {
-  default = "travisci/worker:v4.2.0"
+  default = "travisci/worker:v4.3.0"
 }
 
 variable "worker_image" {}
