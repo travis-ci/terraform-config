7c00bc7a80b0e09c4e434c895c390ee84c702409
Dan Buch <dan@meatballhat.com>
2018-11-28 17:19:50 -0500

    Add autoscaler to build-cache instance group

--- modules/gce_squignix/main.tf
+++ modules/gce_squignix/main.tf
@@ -2,7 +2,19 @@ variable "allowed_internal_ranges" {
   default = ["10.0.0.0/8"]
 }
 
-variable "backend_ig_size" {
+variable "backend_ig_cooldown_period" {
+  default = 60
+}
+
+variable "backend_ig_lb_utilization" {
+  default = 0.9
+}
+
+variable "backend_ig_max_size" {
+  default = 5
+}
+
+variable "backend_ig_min_size" {
   default = 2
 }
 
@@ -166,6 +178,26 @@ resource "google_compute_instance_template" "build_cache" {
   }
 }
 
+resource "google_compute_region_autoscaler" "build_cache" {
+  name   = "${var.env}-${var.index}-build-cache"
+  region = "${var.region}"
+  target = "${google_compute_region_instance_group_manager.build_cache.self_link}"
+
+  autoscaling_policy = {
+    max_replicas    = "${var.backend_ig_max_size}"
+    min_replicas    = "${var.backend_ig_min_size}"
+    cooldown_period = "${var.backend_ig_cooldown_period}"
+
+    load_balancing_utilization {
+      target = "${var.backend_ig_lb_utilization}"
+    }
+  }
+}
+
+resource "google_compute_target_pool" "build_cache" {
+  name = "${var.env}-${var.index}-build-cache"
+}
+
 resource "google_compute_region_instance_group_manager" "build_cache" {
   provider = "google-beta"
 
@@ -173,7 +205,7 @@ resource "google_compute_region_instance_group_manager" "build_cache" {
   distribution_policy_zones = "${formatlist("${var.region}-%s", var.zones)}"
   name                      = "${var.env}-${var.index}-build-cache"
   region                    = "${var.region}"
-  target_size               = "${var.backend_ig_size}"
+  target_pools              = ["${google_compute_target_pool.build_cache.self_link}"]
 
   version {
     name              = "default"
