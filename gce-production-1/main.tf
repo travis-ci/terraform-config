ae8ecd09127cdc041a7c5a67b893d85966a0178b
Dan Buch <dan@meatballhat.com>
2018-12-06 16:13:22 -0500

    Remove busted prod 3 references

--- gce-production-1/main.tf
+++ gce-production-1/main.tf
@@ -83,17 +83,6 @@ data "terraform_remote_state" "production_2" {
   }
 }
 
-data "terraform_remote_state" "production_3" {
-  backend = "s3"
-
-  config {
-    bucket         = "travis-terraform-state"
-    key            = "terraform-config/gce-production-3.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "travis-terraform-state"
-  }
-}
-
 module "aws_iam_user_s3_com" {
   source = "../modules/aws_iam_user_s3"
 
@@ -196,11 +185,4 @@ resource "google_project_iam_member" "production_2_workers" {
   project = "${var.project}"
   role    = "roles/compute.imageUser"
   member  = "serviceAccount:${element(data.terraform_remote_state.production_2.workers_service_account_emails, count.index)}"
-}
-
-resource "google_project_iam_member" "production_3_workers" {
-  count   = "${length(data.terraform_remote_state.production_3.workers_service_account_emails)}"
-  project = "${var.project}"
-  role    = "roles/compute.imageUser"
-  member  = "serviceAccount:${element(data.terraform_remote_state.production_3.workers_service_account_emails, count.index)}"
 }
