028e12540d35fbdc6ce4a66cb6b3adc8baf27437
Dan Buch <dan@meatballhat.com>
2018-12-07 00:05:38 -0500

    Fix heroku client bits
    to reintroduce the methods used by bin/heroku-wait-deploy-scale

--- lib/heroku_client.rb
+++ lib/heroku_client.rb
@@ -15,6 +15,74 @@ class HerokuClient
   private :api_key
   private :api_host
 
+  def wait(heroku_app, timeout: 120)
+    c = 0
+
+    loop do
+      return true if get("/apps/#{heroku_app}")
+      return false if c >= timeout
+
+      sleep 10
+      c += 10
+    end
+
+    true
+  end
+
+  def deploy(repo_slug, heroku_app, version)
+    warn "\nDeploying #{repo_slug} #{version} to #{heroku_app}"
+
+    response = post(
+      JSON.generate(
+        'source_blob' => {
+          'url' => "https://github.com/#{repo_slug}/archive/#{version}.tar.gz",
+          'version' => version
+        }
+      ),
+      "/apps/#{heroku_app}/builds"
+    )
+
+    raise 'Could not request a deployment' if response.nil?
+
+    warn "\nStreaming deploy output"
+
+    raise 'Could not stream deployment output' unless stream(URI(response.fetch('output_stream_url')))
+  end
+
+  def scale(heroku_app, ps_scales)
+    Array(ps_scales).each do |ps_scale|
+      formation = parse_formation(ps_scale)
+      response = patch(
+        JSON.generate(
+          'quantity' => formation.fetch(:qty),
+          'size' => formation.fetch(:size)
+        ),
+        "/apps/#{heroku_app}/formation/#{formation.fetch(:type)}"
+      )
+      raise "Could not scale #{heroku_app} #{ps_scales.inspect}" if response.nil?
+
+      warn "---> scaled #{heroku_app} #{ps_scale}:"
+      warn JSON.pretty_generate(response)
+    end
+  end
+
+  def parse_formation(ps_scale)
+    ret = {
+      type: '',
+      qty: 0,
+      size: ''
+    }
+
+    parts = ps_scale.split('=', 2)
+    ret[:type] = parts.fetch(0)
+
+    subparts = parts.fetch(1).split(':', 2)
+    ret[:qty] = Integer(subparts.fetch(0))
+    ret[:size] = subparts.fetch(1).strip
+
+    ret
+  end
+
   def get(path)
     request(Net::HTTP::Get.new(path))
   end
