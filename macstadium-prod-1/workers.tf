6d7c1f53032da8e33bf9b893edd98fa766ae40d7
Matt Moriarity <matt@mattmoriarity.com>
2019-01-21 15:03:47 +0100

    Bring down another .com worker in macstadium-prod-1

--- macstadium-prod-1/workers.tf
+++ macstadium-prod-1/workers.tf
@@ -159,32 +159,6 @@ export AWS_SECRET_ACCESS_KEY=${module.aws_iam_user_s3_com.secret}
 EOF
 }
 
-module "worker_production_com_3" {
-  source             = "../modules/macstadium_go_worker"
-  host_id            = "${module.macstadium_infrastructure.wjb_uuid}"
-  ssh_host           = "${module.macstadium_infrastructure.wjb_ip}"
-  ssh_user           = "${var.ssh_user}"
-  worker_version     = "${var.travis_worker_version}"
-  env                = "production-com-3"
-  index              = "${var.index}"
-  worker_base_config = "${data.template_file.worker_config_common.rendered}"
-  worker_env_config  = "${file("${path.module}/config/travis-worker-production-com-common")}"
-
-  worker_local_config = <<EOF
-export TRAVIS_WORKER_HARD_TIMEOUT=120m
-export TRAVIS_WORKER_TRAVIS_SITE="com"
-export TRAVIS_WORKER_POOL_SIZE="${var.worker_com_pool_size}"
-export TRAVIS_WORKER_PPROF_PORT="7082"
-export TRAVIS_WORKER_HTTP_API_AUTH="macstadium-worker:${random_id.travis_worker_production_com_token.hex}"
-export TRAVIS_WORKER_JUPITERBRAIN_ENDPOINT="http://${random_id.jupiter_brain_production_com_token.hex}@127.0.0.1:8083/"
-export TRAVIS_WORKER_LIBRATO_SOURCE="travis-worker-production-com-macstadium-${var.index}-3-dc18"
-
-export TRAVIS_WORKER_BUILD_TRACE_S3_BUCKET=${module.aws_iam_user_s3_com.bucket}
-export AWS_ACCESS_KEY_ID=${module.aws_iam_user_s3_com.id}
-export AWS_SECRET_ACCESS_KEY=${module.aws_iam_user_s3_com.secret}
-EOF
-}
-
 module "worker_custom_1" {
   source             = "../modules/macstadium_go_worker"
   host_id            = "${module.macstadium_infrastructure.wjb_uuid}"
