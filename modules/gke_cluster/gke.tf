5a42f49302cb6ed19dfea05dc982240bf4224dd5
emdantrim <emdantrim@gmail.com>
2019-01-25 19:12:47 +0100

    terraform fmt

--- modules/gke_cluster/gke.tf
+++ modules/gke_cluster/gke.tf
@@ -7,27 +7,27 @@ variable "zone" {
 }
 
 resource "google_container_cluster" "gke_cluster" {
-  name               = "${var.name}"
-  zone               = "${var.zone}"
-  min_master_version = "1.11"
-  initial_node_count = 1
+  name                     = "${var.name}"
+  zone                     = "${var.zone}"
+  min_master_version       = "1.11"
+  initial_node_count       = 1
   remove_default_node_pool = true
 
   master_auth {
     client_certificate_config {
       issue_client_certificate = false
     }
+
     username = "${random_id.username.hex}"
     password = "${random_id.password.hex}"
   }
-
 }
 
 resource "google_container_node_pool" "node_pool" {
   name               = "${var.name}"
   zone               = "${var.zone}"
   initial_node_count = 1
-  cluster = "${google_container_cluster.gke_cluster.name}"
+  cluster            = "${google_container_cluster.gke_cluster.name}"
 
   node_config {
     oauth_scopes = [
@@ -37,17 +37,16 @@ resource "google_container_node_pool" "node_pool" {
       "https://www.googleapis.com/auth/monitoring",
     ]
   }
-  
+
   management {
     auto_upgrade = true
-    auto_repair = true
+    auto_repair  = true
   }
 
   autoscaling {
     min_node_count = 1
     max_node_count = 3
   }
-
 }
 
 resource "random_id" "username" {
@@ -57,7 +56,6 @@ resource "random_id" "username" {
 resource "random_id" "password" {
   byte_length = 8
 }
-
 
 # The following outputs allow authentication and connectivity to the GKE Cluster.
 output "endpoint" {
