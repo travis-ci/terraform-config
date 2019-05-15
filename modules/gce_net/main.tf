fae34dc0a18f2c1179dca92d2172ba6bdc698801
Duologic <jeroen@simplistic.be>
2019-05-15 10:59:14 +0200

    Use google-beta on gc_instance_group_manager.net
    This resolves:
    
    Error: module.gce_net.google_compute_instance_group_manager.nat[3]:
    "auto_healing_policies.0.initial_delay_sec": [REMOVED] This field is in
    beta. Use it in the the google-beta provider instead. See
    https://terraform.io/docs/providers/google/provider_versions.html for
    more details.

--- modules/gce_net/main.tf
+++ modules/gce_net/main.tf
@@ -368,14 +368,18 @@ resource "google_compute_firewall" "allow_nat_health_check" {
 }
 
 resource "google_compute_instance_group_manager" "nat" {
-  count = "${length(var.nat_zones) * var.nat_count_per_zone}"
+  provider = "google-beta"
+  count    = "${length(var.nat_zones) * var.nat_count_per_zone}"
 
   base_instance_name = "${var.env}-${var.index}-nat-${element(var.nat_zones, count.index / var.nat_count_per_zone)}-${(count.index % var.nat_count_per_zone) + 1}"
-  instance_template  = "${element(google_compute_instance_template.nat.*.self_link, count.index)}"
   name               = "nat-${element(var.nat_zones, count.index / var.nat_count_per_zone)}-${(count.index % var.nat_count_per_zone) + 1}"
   target_size        = 1
-  update_strategy    = "NONE"
   zone               = "${var.region}-${element(var.nat_zones, count.index / var.nat_count_per_zone)}"
+
+  version {
+    name              = "${var.env}-${var.index}-nat-default"
+    instance_template = "${element(google_compute_instance_template.nat.*.self_link, count.index)}"
+  }
 
   named_port {
     name = "http"
