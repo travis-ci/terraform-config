7dd2778d8ebc917e9a5a96e16d6c9cffad2397d7
Duologic <jeroen@simplistic.be>
2019-08-02 13:04:23 +0200

    Refactor Makefiles for GCE
    As Terraform doesn't configure the worker VMs anymore, there is no need
    to write-config-files, speeding up the TF targets.
    
    On the gce-*-net-*/ bastion still needs some config, so keeping it
    around for now. Removing/replacing bastion is out-of-scope.

--- gce.mk
+++ gce.mk
@@ -2,39 +2,3 @@ TOP := $(shell git rev-parse --show-toplevel)
 
 include $(TOP)/terraform-common.mk
 include $(TOP)/trvs.mk
-
-WRITE_CONFIG_OPTS := --write-bastion --env-tail $(ENV_TAIL)
-
-.PHONY: default
-default: hello
-
-CONFIG_FILES := \
-	config/bastion.env \
-	config/gce-workers-$(ENV_SHORT).json \
-	config/worker-com.env \
-	config/worker-org.env
-
-.PHONY: .config
-.config: $(CONFIG_FILES) $(ENV_NAME).auto.tfvars
-
-$(CONFIG_FILES): config/.written config/.gce-keys-written
-
-# Imports network resources from a GCE project that used a single terraform
-# graph to manage network resources and workers into a separate network-only
-# graph a la "gce-staging-net-1" for "gce-staging-1".  This target is intended
-# to be run within a given "net" graph directory such as "gce-production-net-5".
-.PHONY: import-net
-import-net:
-	$(TOP)/bin/gce-import-net \
-		--env $(ENV_SHORT) \
-		--index $(shell awk -F- '{ print $$NF }' <<<$(ENV_NAME)) \
-		--project $(shell $(TOP)/bin/lookup-gce-project $(ENV_NAME)) \
-		--terraform $(TERRAFORM)
-
-# Removes state references from a GCE project that has migrated network
-# resources to a network-only terraform graph (see `import-net` above). This
-# target is intended to be run within a given "non-net" graph directory such as
-# "gce-production-5".
-.PHONY: export-net
-export-net:
-	$(TOP)/bin/gce-export-net --terraform $(TERRAFORM)
