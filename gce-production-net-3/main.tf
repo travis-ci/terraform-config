3b22e8d9b95204abdc0d50a60534da7641268ff1
Duologic <jeroen@simplistic.be>
2019-08-07 09:14:35 +0200

    Revert "Extensive cleanup after instance NAT removal"
    This reverts commit 2cd156d52c6ac411d1c083ec7489f4213f60a47e.

--- gce-production-net-3/main.tf
+++ gce-production-net-3/main.tf
@@ -10,10 +10,24 @@ variable "gce_bastion_image" {
   default = "https://www.googleapis.com/compute/v1/projects/eco-emissary-99515/global/images/bastion-1519767738-74530dd"
 }
 
+variable "gce_heroku_org" {}
+
+variable "gce_nat_image" {
+  default = "https://www.googleapis.com/compute/v1/projects/eco-emissary-99515/global/images/tfw-1520467760-573cd26"
+}
+
 variable "github_users" {}
 
 variable "index" {
   default = 3
+}
+
+variable "nat_conntracker_src_ignore" {
+  type = "list"
+}
+
+variable "nat_conntracker_dst_ignore" {
+  type = "list"
 }
 
 variable "project" {
@@ -53,13 +67,21 @@ provider "heroku" {}
 module "gce_net" {
   source = "../modules/gce_net"
 
-  bastion_config                = "${file("config/bastion.env")}"
-  bastion_image                 = "${var.gce_bastion_image}"
-  deny_target_ip_ranges         = ["${var.deny_target_ip_ranges}"]
-  env                           = "${var.env}"
+  bastion_config        = "${file("config/bastion.env")}"
+  bastion_image         = "${var.gce_bastion_image}"
+  deny_target_ip_ranges = ["${var.deny_target_ip_ranges}"]
+  env                   = "${var.env}"
+
   github_users                  = "${var.github_users}"
+  heroku_org                    = "${var.gce_heroku_org}"
   index                         = "${var.index}"
+  nat_config                    = "${file("config/nat.env")}"
+  nat_conntracker_config        = "${file("nat-conntracker.env")}"
+  nat_conntracker_dst_ignore    = ["${var.nat_conntracker_dst_ignore}"]
+  nat_conntracker_src_ignore    = ["${var.nat_conntracker_src_ignore}"]
   nat_count_per_zone            = 1
+  nat_image                     = "${var.gce_nat_image}"
+  nat_machine_type              = "n1-standard-4"
   project                       = "${var.project}"
   public_subnet_cidr_range      = "10.10.1.0/24"
   rigaer_strasse_8_ipv4         = "${var.rigaer_strasse_8_ipv4}"
