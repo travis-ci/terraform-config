ab79394b27ef7f931967ddf66bff3eb523841008
Duologic <jeroen@simplistic.be>
2019-08-19 14:55:27 +0200

    Initialize new project

---
+++ travis-ci-prod-services-1/variables.tf
@@ -0,0 +1,11 @@
+variable "project_name" {
+  default = "Production Services 1"
+}
+
+variable "project_id" {
+  default = "travis-ci-prod-services-1"
+}
+
+variable "region" {
+  default = "us-central1"
+}
