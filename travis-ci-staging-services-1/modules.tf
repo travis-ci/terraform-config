d18edb4f0528a18d0d0786ceee2e70f5dffc2bda
Duologic <jeroen@simplistic.be>
2019-08-20 10:16:26 +0200

    Create staging setup for services

---
+++ travis-ci-staging-services-1/modules.tf
@@ -0,0 +1,28 @@
+module "project" {
+  source = "../modules/gce_project"
+
+  project_name = "${var.project_name}"
+  project_id   = "${var.project_id}"
+}
+
+module "networking" {
+  source = "../modules/gce_net_services"
+
+  project = "${module.project.project_id}"
+}
+
+module "kubernetes_cluster" {
+  source = "../modules/gce_kubernetes"
+
+  cluster_name      = "travis-ci-services"
+  default_namespace = "default"
+  network           = "${module.networking.main_network_name}"
+  pool_name         = "pool1"
+  project           = "${module.project.project_id}"
+  subnetwork        = "${module.networking.services_network_name}"
+
+  node_pool_tags       = ["services"]
+  max_node_count       = 10
+  machine_type         = "c2-standard-4"
+  enable_private_nodes = true
+}
---
+++ travis-ci-staging-services-1/variables.tf
@@ -0,0 +1,11 @@
+variable "project_name" {
+  default = "Staging Services 1"
+}
+
+variable "project_id" {
+  default = "travis-ci-staging-services-1"
+}
+
+variable "region" {
+  default = "us-central1"
+}
