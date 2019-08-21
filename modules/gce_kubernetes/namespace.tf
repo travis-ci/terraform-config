ec31d3442636288f42f00c5427733374e3653015
Duologic <jeroen@simplistic.be>
2019-08-21 16:38:36 +0200

    Don't create default namespace

--- modules/gce_kubernetes/namespace.tf
+++
@@ -1,9 +0,0 @@
-resource "kubernetes_namespace" "default" {
-  metadata {
-    name = "${var.default_namespace}"
-  }
-}
-
-output "kubernetes_default_namespace" {
-  value = "${kubernetes_namespace.default.metadata.name}"
-}
