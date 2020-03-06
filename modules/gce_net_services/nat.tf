b6a7afe2819db645434f3e06124e0c327974c7b1
Damian Szymanski <damian@assembla.com>
2020-03-06 09:00:43 +0100

    Add us-east4 and us-east1 networks

--- modules/gce_net_services/nat.tf
+++ modules/gce_net_services/nat.tf
@@ -4,10 +4,46 @@ resource "google_compute_address" "services_nat" {
   project = "${var.project}"
 }
 
+resource "google_compute_address" "services_nat_us_east1" {
+  count   = "${var.nat_ip_count}"
+  name    = "services-nat-ip-us-east1-${count.index}"
+  project = "${var.project}"
+  region  = "us-east1"
+}
+
+resource "google_compute_address" "services_nat_us_east4" {
+  count   = "${var.nat_ip_count}"
+  name    = "services-nat-ip-us-east4-${count.index}"
+  project = "${var.project}"
+  region  = "us-east4"
+}
+
 resource "google_compute_router" "services_nat" {
   name    = "router"
   project = "${var.project}"
   network = "${google_compute_network.main.self_link}"
+
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router" "services_nat_us_east1" {
+  name    = "router"
+  project = "${var.project}"
+  network = "${google_compute_network.main.self_link}"
+  region  = "us-east1"
+
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router" "services_nat_us_east4" {
+  name    = "router"
+  project = "${var.project}"
+  network = "${google_compute_network.main.self_link}"
+  region  = "us-east4"
 
   bgp {
     asn = 64514
@@ -25,6 +61,38 @@ resource "google_compute_router_nat" "services_nat" {
 
   subnetwork {
     name                    = "${google_compute_subnetwork.services.self_link}"
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+}
+
+resource "google_compute_router_nat" "services_nat_us_east1" {
+  name    = "services-nat-us-east1"
+  project = "${var.project}"
+  region  = "us-east1"
+
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+  nat_ips                            = ["${google_compute_address.services_nat_us_east1.*.self_link}"]
+  router                             = "${google_compute_router.services_nat_us_east1.name}"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+
+  subnetwork {
+    name                    = "${google_compute_subnetwork.services-us-east1.self_link}"
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+}
+
+resource "google_compute_router_nat" "services_nat_us_east4" {
+  name    = "services-nat-us-east4"
+  project = "${var.project}"
+  region  = "us-east4"
+
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+  nat_ips                            = ["${google_compute_address.services_nat_us_east4.*.self_link}"]
+  router                             = "${google_compute_router.services_nat_us_east4.name}"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+
+  subnetwork {
+    name                    = "${google_compute_subnetwork.services-us-east4.self_link}"
     source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
   }
 }
--- modules/gce_net_services/networks.tf
+++ modules/gce_net_services/networks.tf
@@ -13,10 +13,38 @@ resource "google_compute_subnetwork" "services" {
   private_ip_google_access = "true"
 }
 
+resource "google_compute_subnetwork" "services-us-east1" {
+  name                     = "services-us-east1"
+  region                   = "us-east1"
+  project                  = "${var.project}"
+  ip_cidr_range            = "${var.services_subnet_cidr_range_us_east1}"
+  network                  = "${google_compute_network.main.self_link}"
+  enable_flow_logs         = "true"
+  private_ip_google_access = "true"
+}
+
+resource "google_compute_subnetwork" "services-us-east4" {
+  name                     = "services-us-east4"
+  region                   = "us-east4"
+  project                  = "${var.project}"
+  ip_cidr_range            = "${var.services_subnet_cidr_range_us_east4}"
+  network                  = "${google_compute_network.main.self_link}"
+  enable_flow_logs         = "true"
+  private_ip_google_access = "true"
+}
+
 output "main_network_name" {
   value = "${google_compute_network.main.name}"
 }
 
 output "services_network_name" {
   value = "${google_compute_subnetwork.services.name}"
+}
+
+output "services_network_name_us_east1" {
+  value = "${google_compute_subnetwork.services-us-east1.name}"
+}
+
+output "services_network_name_us_east4" {
+  value = "${google_compute_subnetwork.services-us-east4.name}"
 }
